f71568f0158701961f439d3d0ee80641b4f13043
PoChuanSu <peterpeter1010@live.com>
2025-06-25 17:54:06 +1000

    header / footer finish

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,19 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+
 function App() {
-    return <h1>Welcome to PassShop</h1>;
+    return (
+        <>
+            <Header />
+            <main>
+                <Container className="py-3">
+                    <h1>Welcome to PassShop</h1>
+                </Container>
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
